f1f1f8bf8072600826fb07742c17c77eb1bc6bb0
MicheleLucioBasso <michelelucio.basso@mail.polimi.it>
2025-11-02 17:41:32 +0100

    Completamento challenge4

--- MicheleLucioBasso/challenge4/sketch.js
+++ MicheleLucioBasso/challenge4/sketch.js
@@ -17,13 +17,53 @@ function preload() {
 function setup() {
 
   createCanvas(windowWidth, windowHeight);
+
+  let allTempo = [
+    ...tableAlfa.getColumn('timestamp'), //... davanti a array in parentesi quadre [], l'operatore "espande" o "scompatta" tutti elementi di quell'array
+                                         //getColumn('timestamp') estrae tutti valori nella colonna timestamp in array allTempo
+    ...tableBravo.getColumn('timestamp'),
+    ...tableCharlie.getColumn('timestamp')
+  ];
+
+  let allZ = [
+    ...tableAlfa.getColumn('z_pos'), //getColumn('z_pos') estrae tutti valori nella colonna z_pos in array allZ
+    ...tableBravo.getColumn('z_pos'),
+    ...tableCharlie.getColumn('z_pos')
+  ];
   
-  minTempo = min(tableAlfa.getColumn('timestamp')); //valori minimi e massimi di tempo e posizione Z
-  maxTempo = max(tableAlfa.getColumn('timestamp'));
+  minTempo = min(allTempo); //valori minimi e massimi di tempo e posizione Z
+  maxTempo = max(allTempo);
   
-  minZ = min(tableAlfa.getColumn('z_pos')); 
-  maxZ = max(tableAlfa.getColumn('z_pos'));
+  minZ = min(allZ); 
+  maxZ = max(allZ);
   
+}
+
+function drawDroneLine(dataTable, colorDrone) { //disegna linea di un drone ricevendo tabella dati e colore
+
+  let areaGraficoW = width - 2 * MARGINE; //calcola area dove disegnare linea grafico
+  let areaGraficoH = height - 2 * MARGINE;
+
+  noFill();
+  stroke(colorDrone); //imposta colore linea
+  strokeWeight(2); 
+  
+  beginShape(); //inizia a tracciare linea, aggiunge vertici a forma personalizzata
+
+  for (let i = 0; i < dataTable.getRowCount(); i++) { //itera su ogni riga dati
+                                                      //inizia da i=0
+                                                      //i < dataTable.getRowCount() restituisce numero totale righe (punti dati) nel file CSV, ciclo continua finché elaborato ultima riga
+    let tempo = dataTable.getNum(i, 'timestamp'); //i è numero riga corrente, 'timestamp' nome colonna tempo drone
+    let zPos = dataTable.getNum(i, 'z_pos'); //i è numero riga corrente, 'z_pos' nome colonna posizione Z drone
+        
+    let x = map(tempo, minTempo, maxTempo, MARGINE, MARGINE + areaGraficoW); //asse X
+                                                                             //valore originale è tempo, compreso in intervallo tra minTempo e maxTempo, valore viene reinserito proporzionalmente in intervallo tra MARGINE e MARGINE + areaGraficoW
+    let y = map(zPos, minZ, maxZ, MARGINE + areaGraficoH, MARGINE); //asse Y
+                                                                    //valore originale è tempo, compreso in intervallo tra minZ e maxZ, valore viene reinserito proporzionalmente in intervallo tra MARGINE + areaGraficoH e MARGINE
+    vertex(x, y); //aggiunge punto (x, y) a linea grafico
+  }
+  
+  endShape(); //finisce disegnare linea
 }
 
 function draw() {
@@ -33,40 +73,23 @@ function draw() {
   let areaGraficoW = width - 2 * MARGINE; //area dove disegnata linea grafico
   let areaGraficoH = height - 2 * MARGINE;
   
+  noStroke();
   fill("#000000ff");
   textSize(24);
   textAlign(CENTER, TOP);
-  text("Posizione verticale (Z) drone Alfa nel tempo", width / 2, 15);
+  text("Confronto posizione verticale (Z) dei 3 droni nel tempo", width / 2, 15);
   
-  noFill();
-  stroke("#ff0000ff"); //linea rossa
-  strokeWeight(2);
-  
-  beginShape(); //inizia a tracciare linea, aggiunge vertici a forma personalizzata
-  
-  for (let i = 0; i < tableAlfa.getRowCount(); i++) { //itera su ogni riga dati
-                                                      //inizia da i=0
-                                                      //i < tableAlfa.getRowCount() restituisce numero totale righe (punti dati) nel file CSV, ciclo continua finché elaborato ultima riga
-    // getNum() ottiene valori grezzi
-    let tempo = tableAlfa.getNum(i, 'timestamp'); //i è numero riga corrente, 'timestamp' nome colonna tempo drone
-    let zPos = tableAlfa.getNum(i, 'z_pos'); //i è numero riga corrente, 'z_pos' nome colonna posizioneZ drone
-    
-    // 2. Mappa i valori dei dati alle coordinate del canvas (pixel)
-    
-    let x = map(tempo, minTempo, maxTempo, MARGINE, MARGINE + areaGraficoW); //asse X
-                                                                             //valore originale è tempo, compreso in intervallo tra minTempo e maxTempo, valore viene reinserito proporzionalmente in intervallo tra MARGINE e MARGINE + areaGraficoW
-    
-    // Asse Y (Posizione Z): Mappa da [minZ, maxZ] a [MARGINE + areaGraficoH, MARGINE]
-    // L'asse Y è invertito in p5.js (0 è in alto), per questo i limiti sono invertiti.
-    let y = map(zPos, minZ, maxZ, MARGINE + areaGraficoH, MARGINE); //asse y
-                                                                    //valore originale è posZ, compreso in intervallo tra minZ e maxZ, valore viene reinserito proporzionalmente in intervallo tra MARGINE + areaGraficoH e MARGINE
-    
-    vertex(x, y); //aggiunge punto (x, y) a linea grafico
+  noStroke();
+  textSize(14);
+  textAlign(RIGHT, TOP);
+  fill("#ff0000ff"); text("Drone Alfa", width - MARGINE, 50);
+  fill("#003cffff"); text("Drone Bravo", width - MARGINE, 70);
+  fill("#13ba23ff"); text("Drone Charlie", width - MARGINE, 90);
 
-  }
-  
-  endShape(); //finisce tracciare linea
-    
+  drawDroneLine(tableAlfa, "#ff0000ff"); //linea drone Alfa
+  drawDroneLine(tableBravo, "#003cffff"); //linea drone Bravo
+  drawDroneLine(tableCharlie, "#13ba23ff"); //linea drone Charlie
+
   stroke("#000000ff"); //assi grafico
   strokeWeight(1);
   
@@ -74,6 +97,7 @@ function draw() {
   
   line(MARGINE, MARGINE, MARGINE, height - MARGINE); //asseY posizione Z
   
+  noStroke();
   fill("#000000ff");
   textSize(14);
   textAlign(CENTER, TOP);
@@ -87,7 +111,7 @@ function draw() {
   text("Posizione Z (m)", 0, 0);
 
   pop();
-  
+
   textSize(10);
   fill("#000000ff");
   
